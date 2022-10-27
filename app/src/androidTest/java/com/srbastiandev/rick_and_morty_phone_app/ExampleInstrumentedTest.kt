17b8319c237ea196d05f2cf42b400ed4aea03d57
Tenki <srodriguezdev.l@gmail.com>
2022-10-26 21:09:19 -0500

    initial Commit

---
+++ app/src/androidTest/java/com/srbastiandev/rick_and_morty_phone_app/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.srbastiandev.rick_and_morty_phone_app
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.srbastiandev.rick_and_morty_phone_app", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/srbastiandev/rick_and_morty_phone_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.srbastiandev.rick_and_morty_phone_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
